e72cac283337eae0379fd0f1330c57bff7d6beac
slacki123 <sebastian.lacki1@gmail.com>
2019-01-08 13:20:07 +0000

    wut

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const http = require('http');
 const fs = require('fs');
 const hostname = '127.0.0.1';
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 fs.readFile('myPage.html', (err, html) => {
 	if(err) {
@@ -16,7 +16,7 @@ fs.readFile('myPage.html', (err, html) => {
 		res.end();
 	});
 
-	server.listen(port, hostname, () => {
+	server.listen(port, () => {
 		console.log('Server started on port ' + port);
 		});
 
